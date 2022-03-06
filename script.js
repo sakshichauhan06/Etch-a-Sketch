891def010e7b95fffafac934c3a4b1e96e73da11
Sakshi Chauahan <chauhansakshi4m@gmail.com>
2022-03-06 09:37:51 +0530

    slider changes

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 
-
-function createGrid(x){
-    for(var rows = 0; rows < x; rows++) {
-        for(var col = 0; col < x; col++) {
-            $("#pad").append("<div class='grid'></div>");
-        }
-    }
-    $(".grid").width(450/x);
-    $(".grid").height(450/x);
+function sliderFunc(val) {
+    document.getElementById("value").innerHTML = val;
 }
 
-var gridValue = document.getElementById("slider");
-
-console.log(gridValue.value);

7b757d24e0df465fc63b1081e1ed73633433e2b4
Sakshi Chauahan <chauhansakshi4m@gmail.com>
2022-03-07 17:10:06 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 
-function sliderFunc(val) {
+function sliderFun(val) {
     document.getElementById("value").innerHTML = val;
 }
 
+let pad = document.querySelector('.pad');
+pad.style.gridTemplateColumn = "repeat(16, 1fr)";
+pad.style.gridTemplateRows = "repeat(16, 1fr)";
+
+for(let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.background = "blue";
+    pad.insertAdjacentElement("beforeend", square);
+}

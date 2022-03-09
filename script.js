18147cb579ca1cc1528f28029db9d99d48cb6d34
Sakshi Chauahan <chauhansakshi4m@gmail.com>
2022-03-09 14:01:44 +0530

    dynamic divs

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ function sliderFun(val) {
     document.getElementById("value").innerHTML = val;
 }
 
-let pad = document.querySelector('.pad');
-pad.style.gridTemplateColumn = "repeat(16, 1fr)";
-pad.style.gridTemplateRows = "repeat(16, 1fr)";
+function createGrid(size) {
+    let pad = document.querySelector('.pad');
+    let squares = pad.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    pad.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for(let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.background = "blue";
-    pad.insertAdjacentElement("beforeend", square);
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.style.background = "#f4f1de";
+        square.style.border = "0.3px solid"
+        pad.insertAdjacentElement("beforeend", square);
+    }
 }
+
+createGrid(16);
+
+
